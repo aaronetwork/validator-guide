77c8365bcaa22587ce2ca81fc572e3c2b4dbfd44
Aaron Network <aaronetwork@proton.me>
2025-06-23 19:54:32 +0100

    fix

--- readme.md
+++ readme.md
@@ -71,24 +71,10 @@ ignite chain build
 
 `aaronetworkd init moniker --chain-id aaronetwork`
 
-`curl -L https://raw.githubusercontent.com/aaronetwork/chain-genesis/refs/heads/main/genesis.json -o ~/.aaronetwork/config/genesis.json`
-
 ```shell
-sed -i 's/^minimum-gas-prices *=.*/minimum-gas-prices = "0.001uaaron"/' ~/.aaronetwork/config/app.toml
-sed -i 's|^seeds *=.*|seeds = "dc647a7389d3396b0a0d72d71240b02c30c47ef7@63.250.41.78:26656,162607f091deda607273bf2f66c77e50e1cabf3f@89.110.110.161:26656"|' ~/.aaronetwork/config/config.toml
-sed -i 's|^persistent_peers *=.*|persistent_peers = "dc647a7389d3396b0a0d72d71240b02c30c47ef7@63.250.41.78:26656,162607f091deda607273bf2f66c77e50e1cabf3f@89.110.110.161:26656"|' ~/.aaronetwork/config/config.toml
-sed -i 's|^cors_allowed_origins = .*|cors_allowed_origins = ["*"]|' ~/.aaronetwork/config/config.toml
-sed -i 's|laddr = "tcp://127.0.0.1:26657"|laddr = "tcp://0.0.0.0:26657"|' ~/.aaronetwork/config/config.toml
-sed -i 's|^enable = .*|enable = true|' ~/.aaronetwork/config/config.toml
-sed -i 's|^rpc_servers = .*|rpc_servers = "63.250.41.78:26657,146.103.96.113:26657,146.103.99.198:26657"|' ~/.aaronetwork/config/config.toml
-sed -i 's|^trust_height = .*|trust_height = 3013516|' ~/.aaronetwork/config/config.toml
-sed -i 's|^trust_hash = .*|trust_hash = "C3EEA953DBF43205C5B4DB4103E1409632FCE4CC0C4C1BEEC139A98C2C4BC603"|' ~/.aaronetwork/config/config.toml
-sed -i 's/^pruning = .*/pruning = "custom"/' ~/.aaronetwork/config/app.toml
-sed -i 's/^pruning-keep-recent = .*/pruning-keep-recent = "86400"/' ~/.aaronetwork/config/app.toml
-sed -i 's/^pruning-keep-every = .*/pruning-keep-every = "0"/' ~/.aaronetwork/config/app.toml
-sed -i 's/^pruning-interval = .*/pruning-interval = "100"/' ~/.aaronetwork/config/app.toml
-sed -i 's/^snapshot-interval = .*/snapshot-interval = 1000/' ~/.aaronetwork/config/app.toml
-sed -i 's/^snapshot-keep-recent = .*/snapshot-keep-recent = 2/' ~/.aaronetwork/config/app.toml
+curl -L https://raw.githubusercontent.com/aaronetwork/chain-genesis/refs/heads/main/genesis.json -o ~/.aaronetwork/config/genesis.json
+curl -L https://raw.githubusercontent.com/aaronetwork/validator-guide/refs/heads/main/config/config.toml -o ~/.aaronetwork/config/config.toml
+curl -L https://raw.githubusercontent.com/aaronetwork/validator-guide/refs/heads/main/config/app.toml -o ~/.aaronetwork/config/app.toml
 ```
 
 ### Systemctl
--- setup_node.sh
+++ setup_node.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-echo "=== Creating 8G swap ==="
-fallocate -l 8G /swapfile
+echo "=== Creating 6G swap ==="
+fallocate -l 6G /swapfile
 chmod 600 /swapfile
 mkswap /swapfile
 swapon /swapfile
@@ -46,21 +46,8 @@ aaronetworkd init moniker --chain-id aaronetwork
 curl -L https://raw.githubusercontent.com/aaronetwork/chain-genesis/refs/heads/main/genesis.json -o ~/.aaronetwork/config/genesis.json
 
 echo "=== Updating config files ==="
-sed -i 's/^minimum-gas-prices *=.*/minimum-gas-prices = "0.001uaaron"/' ~/.aaronetwork/config/app.toml
-sed -i 's|^seeds *=.*|seeds = "dc647a7389d3396b0a0d72d71240b02c30c47ef7@63.250.41.78:26656,9373f89c3b47a346a6b347208a35079a920511cd@146.103.96.113:26656,a815343c840c2d24e85639b541335103bb0b82a3@146.103.99.198"|' ~/.aaronetwork/config/config.toml
-sed -i 's|^persistent_peers *=.*|persistent_peers = "dc647a7389d3396b0a0d72d71240b02c30c47ef7@63.250.41.78:26656,9373f89c3b47a346a6b347208a35079a920511cd@146.103.96.113:26656,a815343c840c2d24e85639b541335103bb0b82a3@146.103.99.198"|' ~/.aaronetwork/config/config.toml
-sed -i 's|^cors_allowed_origins = .*|cors_allowed_origins = ["*"]|' ~/.aaronetwork/config/config.toml
-sed -i 's|laddr = "tcp://127.0.0.1:26657"|laddr = "tcp://0.0.0.0:26657"|' ~/.aaronetwork/config/config.toml
-sed -i 's|^enable = .*|enable = true|' ~/.aaronetwork/config/config.toml
-sed -i 's|^rpc_servers = .*|rpc_servers = "63.250.41.78:26657,146.103.96.113:26657,146.103.99.198:26657"|' ~/.aaronetwork/config/config.toml
-sed -i 's|^trust_height = .*|trust_height = 3013516|' ~/.aaronetwork/config/config.toml
-sed -i 's|^trust_hash = .*|trust_hash = "C3EEA953DBF43205C5B4DB4103E1409632FCE4CC0C4C1BEEC139A98C2C4BC603"|' ~/.aaronetwork/config/config.toml
-sed -i 's/^pruning = .*/pruning = "custom"/' ~/.aaronetwork/config/app.toml
-sed -i 's/^pruning-keep-recent = .*/pruning-keep-recent = "86400"/' ~/.aaronetwork/config/app.toml
-sed -i 's/^pruning-keep-every = .*/pruning-keep-every = "0"/' ~/.aaronetwork/config/app.toml
-sed -i 's/^pruning-interval = .*/pruning-interval = "100"/' ~/.aaronetwork/config/app.toml
-sed -i 's/^snapshot-interval = .*/snapshot-interval = 1000/' ~/.aaronetwork/config/app.toml
-sed -i 's/^snapshot-keep-recent = .*/snapshot-keep-recent = 2/' ~/.aaronetwork/config/app.toml
+curl -L https://raw.githubusercontent.com/aaronetwork/validator-guide/refs/heads/main/config/config.toml -o ~/.aaronetwork/config/config.toml
+curl -L https://raw.githubusercontent.com/aaronetwork/validator-guide/refs/heads/main/config/app.toml -o ~/.aaronetwork/config/app.toml
 
 echo "=== Creating systemd service ==="
 cat <<EOF | sudo tee /etc/systemd/system/aaronetworkd.service
